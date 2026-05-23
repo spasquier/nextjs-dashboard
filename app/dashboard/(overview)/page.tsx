ac3a7c6ecd1686445814d7df6cfdd42e64844593
Salvador Pasquier <admin@salvadorpasquier.com>
2026-05-22 18:44:10 -0600

    Remove unused variables

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import {
@@ -11,13 +10,6 @@ import {
 } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
